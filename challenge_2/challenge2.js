0d7c8766f2d4ffbaf7ba91915dbac9cba4482626
Miguel Vasquez <vasquezdevelopment@gmail.com>
2021-12-08 15:28:09 -0500

    multiply table

---
+++ challenge_2/challenge2.js
@@ -0,0 +1,21 @@
+// Print a table containing multiplication tables
+
+var result = 'x'
+
+for(var i = 0; i < 11; i++){
+
+    for(var j = 0; j < 11; j++){
+        
+        if(i==0 && j > 0){
+            result += '[' + j + ']';
+        }else if(j == 0 && i > 0){
+            result += '[' + i + ']';
+        }else if(j > 0 && i > 0){
+            result += (i*j) + ' ';
+        }
+    }
+
+    result += '\n';
+}
+
+console.log(result);
