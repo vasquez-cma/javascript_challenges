0a410135ede6a195927154ae2ae702c0c2019aee
Miguel Vasquez <vasquezdevelopment@gmail.com>
2021-12-14 12:19:12 -0500

    Excersise number 7

--- challenge_7/challenge_7.js
+++ challenge_7/challenge_7.js
@@ -1,9 +1,12 @@
-// Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050
-
-let date = new Date();
-
-console.log(date.getFullYear());
+// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 
 
+function lapYear(year){
+    if(year % 4== 0 && year % 100 != 0 || year % 400 == 0 ){
+        console.log("This is a lap year");
+    }else{
+        console.log("This is not a lap year");
+    }
+}
 
 
